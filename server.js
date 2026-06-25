95f9edeb70f5cbc3a076921b91aeab9cb0c7e720
Vrushali Panchal <vrushalipanchal31@gmail.com>
2026-06-25 16:22:22 +0530

    update routes

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const blogRoutes = require("./routes/blogRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 
 dotenv.config();
-console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
-console.log("PORT:", process.env.PORT);
+
 // Connect Database
 connectDB();
 
